0a61640fae76e0ee6ed2f40834aa2b26a40d90b2
Divided by Zer0 <mail@dbzer0.com>
2022-08-30 19:30:46 +0200

    logging

--- server.py
+++ server.py
@@ -513,7 +513,7 @@ class ProcessingGeneration:
         tokens = len(generation.split())
         self.server.record_contribution(tokens, (datetime.now() - self.start_time).seconds)
         self.owner.record_usage()
-        logging.info(f"New Generation delivered by server: {self.name}")
+        logging.info(f"New Generation delivered by server: {self.server.name}")
         return(tokens)
 
     def is_completed(self):
