1ad0fb013ccfefb904b5370ed89ba9ccca24dab9
Divided by Zer0 <mail@dbzer0.com>
2022-08-31 22:32:02 +0200

    update_clientData

--- clientData.py
+++ clientData.py
@@ -1,5 +1,12 @@
-server = "http://dbzer0.com:5001"
-kai = "http://localhost:5000"
+# The cluster url
+cluster_url = "http://dbzer0.com:5001"
+# Where can your bridge reach your KAI instance
+kai_url = "http://localhost:5000"
+# Give a cool name to your instance
 kai_name = "My Awesome Instance"
+# The owner's username. Used for assigning contributions
 username = "username"
 password = "password"
+# Put other users whose prompts you want to prioritize.
+# The owner's username is always included so you don't need to add it here, unless you want it to have lower priority than another user
+priority_usernames = []
