b67c21c62119dd24535899e38f9ea8f82bc94b7c
Divided by Zer0 <mail@dbzer0.com>
2022-09-04 13:20:18 +0200

    test

--- server_classes.py
+++ server_classes.py
@@ -383,6 +383,7 @@ class Database:
         top_contributors = None
         for user in self.contributions:
             self._ensure_contributor_exists(user) # Will remove later
+            logging.info(self.contributions[user])
             if self.contributions[user]['tokens'] > top_contribution:
                 top_contributors = self.get_contributor_entry(user)
                 top_contribution = self.contributions[user]
