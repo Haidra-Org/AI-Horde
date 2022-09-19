6ee94e1d193d43a856f81f96bf3105c9c762301e
Divided by Zer0 <mail@dbzer0.com>
2022-09-19 22:57:24 +0200

    dict should exist

--- server_classes.py
+++ server_classes.py
@@ -344,14 +344,13 @@ class PromptsIndex(Index):
             "queued_requests": 0,
             # mps == Megapixelsteps
             "queued_megapixelsteps": 0,
-            "queued_mps": 0,
         }
         for wp in self._index.values():
             ret_dict["queued_requests"] += wp.n
             if wp.n > 0:
-                ret_dict["queued_mps"] += wp.pixelsteps / 1000000
+                ret_dict["queued_megapixelsteps"] += wp.pixelsteps / 1000000
         # We round the end result to avoid to many decimals
-        ret_dict["queued_mps"] = round(ret_dict["queued_mps"],2)
+        ret_dict["queued_megapixelsteps"] = round(ret_dict["queued_megapixelsteps"],2)
         return(ret_dict)
 
     def get_waiting_wp_by_kudos(self):
