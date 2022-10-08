8d881b619b213299d6f74184f50f2e0c2a6b0312
Divided by Zer0 <mail@dbzer0.com>
2022-10-08 11:32:25 +0200

    split redis from limiter

--- horde/limiter.py
+++ horde/limiter.py
@@ -1,20 +1,16 @@
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from .flask import HORDE
-
-def is_redis_up() -> bool:
-    import socket
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        return s.connect_ex(('localhost', 6379)) == 0
+from . import redis as r
 
 limiter = None
 # Very basic DOS prevention
-if is_redis_up():
+if r.is_redis_up():
     try:
         limiter = Limiter(
             HORDE,
             key_func=get_remote_address,
-            storage_uri="redis://localhost:6379/1",
+            storage_uri=r.ger_limiter_db(),
             # storage_options={"connect_timeout": 30},
             strategy="fixed-window", # or "moving-window"
             default_limits=["90 per minute"]
