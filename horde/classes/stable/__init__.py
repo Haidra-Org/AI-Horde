74b55ffc849813ffd9b2dffa3da0f72d9f03f4c0
Divided by Zer0 <mail@dbzer0.com>
2022-10-22 15:47:41 +0200

    news

--- horde/classes/stable/__init__.py
+++ horde/classes/stable/__init__.py
@@ -228,7 +228,12 @@ class News(News):
     STABLE_HORDE_NEWS = [
         {
             "date_published": "2022-10-22",
-            "newspiece": "We have [a new npm SDK package](https://github.com/ZeldaFan0225/stable_horde) for using the Stable Horde. All kudos to ZeldaFan0225!",
+            "newspiece": "We have [a new npm SDK](https://github.com/ZeldaFan0225/stable_horde) for integrating into the Stable Horde.",
+            "importance": "Information"
+        },
+        {
+            "date_published": "2022-10-22",
+            "newspiece": "Krita and GIMP plugins now support img2img",
             "importance": "Information"
         },
         {
