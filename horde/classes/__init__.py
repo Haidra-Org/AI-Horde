06b3c8e6a34bb0572740f7904590b741a1d355da
Divided by Zer0 <mail@dbzer0.com>
2022-11-27 13:47:57 +0100

    removed_unnecessayImport

--- horde/classes/__init__.py
+++ horde/classes/__init__.py
@@ -8,15 +8,13 @@ from horde.classes.base import stats
 # Should figure out an elegant way to do this with a for loop
 # stats = import_module(name=f'horde.classes.{args.horde}').stats
 try:
-    ProcessingGeneration = import_module(name=f'horde.classes.{args.horde}.processing_generation').ProcessingGeneration
-    ProcessingGenerationExtended = import_module(name=f'horde.classes.{args.horde}.processing_generation').ProcessingGenerationExtended
-    print(f'Loaded ProcessingGenerationExtended')
+    ProcessingGeneration = import_module(name=f'horde.classes.{args.horde}.processing_generation').ProcessingGenerationExtended
+    logger.debug(f'Loaded ProcessingGenerationExtended')
 except (ModuleNotFoundError,AttributeError):
     ProcessingGeneration = import_module(name=f'horde.classes.base.processing_generation').ProcessingGeneration
 try:
-    WaitingPrompt = import_module(name=f'horde.classes.base.waiting_prompt').WaitingPrompt
-    WaitingPromptExtended = import_module(name=f'horde.classes.{args.horde}.waiting_prompt').WaitingPromptExtended
-    print("Loaded WaitingPromptExtended")
+    WaitingPrompt = import_module(name=f'horde.classes.{args.horde}.waiting_prompt').WaitingPromptExtended
+    logger.debug("Loaded WaitingPromptExtended")
 except (ModuleNotFoundError,AttributeError):
     WaitingPrompt = import_module(name=f'horde.classes.base.waiting_prompt').WaitingPrompt
 try:
