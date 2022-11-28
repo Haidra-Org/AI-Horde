1bff11f2e28c44de1f809fd1312f2c1af490de82
Divided by Zer0 <mail@dbzer0.com>
2022-11-28 22:17:11 +0100

    test

--- horde/flask.py
+++ horde/flask.py
@@ -10,7 +10,7 @@ cache = None
 HORDE = Flask(__name__)
 HORDE.wsgi_app = ProxyFix(HORDE.wsgi_app, x_for=1)
 # HORDE.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///horde.db"
-HORDE.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2:///postgres@localhost/stablehorde"
+HORDE.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///postgres@localhost/postgres"
 HORDE.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(HORDE)
 db.init_app(HORDE)
