2773638e628f5a75713a3c5b2f1375ca7e2acf53
Divided by Zer0 <mail@dbzer0.com>
2022-11-29 01:00:24 +0100

    params

--- horde/classes/stable/waiting_prompt.py
+++ horde/classes/stable/waiting_prompt.py
@@ -18,6 +18,8 @@ class WaitingPromptExtended(WaitingPrompt):
     @logger.catch(reraise=True)
     def extract_params(self):
         self.n = self.params.pop('n', 1)
+        self.params['width'] = self.params.get('width', 512)
+        self.params['height'] = self.params.get('height', 512)
         self.jobs = self.n 
         # We assume more than 20 is not needed. But I'll re-evalute if anyone asks.
         if self.n > 20:
