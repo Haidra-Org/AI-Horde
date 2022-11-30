e335c0f83d9cc8f010c7a287fbc80c7fa9484445
Divided by Zer0 <mail@dbzer0.com>
2022-11-30 10:17:12 +0100

    faulted

--- horde/classes/database.py
+++ horde/classes/database.py
@@ -227,7 +227,7 @@ def count_waiting_requests(user, models = []):
     ).join(WaitingPrompt).filter(
         WPModels.model.in_(models),
         WaitingPrompt.user_id == user.id,
-        not WaitingPrompt.is_faulted, # or proc_gen is completed or faulted (Db0: no because there's many procgens. If they're all faulted, the WP will be faulted too)
+        not WaitingPrompt.faulted, # or proc_gen is completed or faulted (Db0: no because there's many procgens. If they're all faulted, the WP will be faulted too)
         WaitingPrompt.n >= 1, 
     ).group_by(WPModels.model).count()
 
