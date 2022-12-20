c69a00380af7899b00331cc9d3cb8adf0bb706fe
Divided by Zer0 <mail@dbzer0.com>
2022-12-20 13:18:58 +0100

    100_models

--- horde/classes/base/worker.py
+++ horde/classes/base/worker.py
@@ -187,8 +187,8 @@ class Worker(db.Model):
         db.session.commit()   
 
     def set_models(self, models):
-        # We don't allow more workers to claim they can server more than 50 models atm (to prevent abuse)
-        models = [sanitize_string(model_name[0:60]) for model_name in models]
+        # We don't allow more workers to claim they can server more than 100 models atm (to prevent abuse)
+        models = [sanitize_string(model_name[0:100]) for model_name in models]
         del models[50:]
         models = set(models)
         existing_models = db.session.query(WorkerModel).filter_by(worker_id=self.id)
