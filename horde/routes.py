fdddfa911bc180fcadee5c0bf55064aa51509062
Divided by Zer0 <mail@dbzer0.com>
2023-01-04 14:23:52 +0100

    remove_cache

--- horde/routes.py
+++ horde/routes.py
@@ -23,7 +23,7 @@ dance_return_to = '/'
 
 @logger.catch(reraise=True)
 @HORDE.route('/')
-@cache.cached(timeout=300)
+# @cache.cached(timeout=300)
 def index():
     with open(f'index_{args.horde}.md') as index_file:
         index = index_file.read()
