dd9571324b19c4f842812066965cf338c037b2d6
Divided by Zer0 <mail@dbzer0.com>
2023-01-30 18:10:31 +0100

    fix

--- horde/image.py
+++ horde/image.py
@@ -3,10 +3,12 @@ from io import BytesIO
 from PIL import Image, UnidentifiedImageError
 from horde.logger import logger
 
-
 def convert_b64_to_pil(source_image_b64):
     base64_bytes = source_image_b64.encode('utf-8')
-    img_bytes = base64.b64decode(base64_bytes)
+    try:
+        img_bytes = base64.b64decode(base64_bytes)
+    except Exception:
+        return None
     try:
         image = Image.open(BytesIO(img_bytes))          
     except UnidentifiedImageError as err:
