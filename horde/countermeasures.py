6e27bda238ceb6f9aca42714d0fb3354ccc5b262
Divided by Zer0 <mail@dbzer0.com>
2023-02-26 21:20:48 +0100

    whitelisted

--- horde/countermeasures.py
+++ horde/countermeasures.py
@@ -97,7 +97,10 @@ class CounterMeasures:
 		if current_suspicion is None:
 			current_suspicion = 0
 		current_suspicion = int(current_suspicion)
-		ip_s_r.setex(ipaddr, timedelta(hours=24), current_suspicion + 1)
+		suspicion_timeout = 24
+		if ipaddr in WHITELISTED_SERVICE_IPS:
+			suspicion_timeout = 1
+		ip_s_r.setex(ipaddr, timedelta(hours=suspicion_timeout), current_suspicion + 1)
 		# Fibonacci in seconds FTW!
 		timeout = (current_suspicion + current_suspicion + 1) * 3
 		if ipaddr in WHITELISTED_SERVICE_IPS and timeout > 150:
