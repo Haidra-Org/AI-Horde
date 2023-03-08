dc6499968a0e77a320972437cb3c9b4cc4ecd17d
Divided by Zer0 <mail@dbzer0.com>
2023-03-08 15:15:34 +0100

    test

--- horde/apis/v2/base.py
+++ horde/apis/v2/base.py
@@ -388,7 +388,7 @@ class JobPopTemplate(Resource):
                 raise e.Profanity(self.user.get_unique_alias(), self.worker_name, 'worker name')
             if is_profane(self.args.bridge_agent):
                 raise e.Profanity(self.user.get_unique_alias(), self.args.bridge_agent, 'bridge agent')
-            colab_search = re.compile(r"\bcolab|\btpu\b", re.IGNORECASE)
+            colab_search = re.compile(r"\b(colab|tpu\b|google)", re.IGNORECASE)
             logger.debug(self.worker_name, colab_search, colab_search.search(self.worker_name))
             if colab_search.search(self.worker_name):
                 raise e.BadRequest("To avoid unwanted attention, please do not use 'Colab' or 'TPU' in your worker names.")
