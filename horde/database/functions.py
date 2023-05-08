36a16239ba1237f21376b003014d95e76f086e27
db0 <mail@dbzer0.com>
2023-05-08 13:38:02 +0200

    fix

--- horde/database/functions.py
+++ horde/database/functions.py
@@ -338,7 +338,7 @@ def retrieve_available_models(model_type=None,min_count=None,max_count=None):
 
 def transfer_kudos(source_user, dest_user, amount):
     reverse_transfer = hr.horde_r_get(f'kudos_transfer_{dest_user.id}-{source_user.id}')
-    if bool(int(reverse_transfer)):
+    if reverse_transfer:
         return([0,'This user transferred kudos to you very recently. Please wait at least 1 minute.'])
     if source_user.is_suspicious():
         return([0,'Something went wrong when sending kudos. Please contact the mods.'])
@@ -352,7 +352,7 @@ def transfer_kudos(source_user, dest_user, amount):
         return([0,'Nice try...'])
     if amount > source_user.kudos - source_user.get_min_kudos():
         return([0,'Not enough kudos.'])
-    hr.horde_r_setex(f'kudos_transfer_{source_user.id}-{dest_user.id}', timedelta(seconds=60), int(True))
+    hr.horde_r_setex(f'kudos_transfer_{source_user.id}-{dest_user.id}', timedelta(seconds=60), 1)
     transfer_log = KudosTransferLog.query.filter_by(
         source_id = source_user.id,
         dest_id = dest_user.id,
