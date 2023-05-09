88bb4d1f144e3a15b47002f205d22c52dd7a6ebf
db0 <mail@dbzer0.com>
2023-05-10 00:40:13 +0200

    avoid_double_calling_model

--- horde/apis/v2/base.py
+++ horde/apis/v2/base.py
@@ -142,10 +142,12 @@ class GenerateTemplate(Resource):
         #logger.warning(datetime.utcnow())
         self.initiate_waiting_prompt()
         #logger.warning(datetime.utcnow())
-        self.kudos = self.wp.extrapolate_dry_run_kudos()
         if self.args.dry_run:
+            self.kudos = self.wp.extrapolate_dry_run_kudos()
             return
         self.activate_waiting_prompt()
+        # We use the wp.kudos to avoid calling the model twice.
+        self.kudos = self.wp.kudos
         #logger.warning(datetime.utcnow())
 
     # We split this into its own function, so that it may be overriden and extended
