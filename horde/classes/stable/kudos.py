d23390610bca3900dd0c1d27de3364d5d24b3131
tazlin <tazlin.on.github@gmail.com>
2023-05-09 12:12:42 -0400

    fix: add default value back to kudos load_model

--- horde/classes/stable/kudos.py
+++ horde/classes/stable/kudos.py
@@ -202,7 +202,7 @@ class KudosModel:
             dim=1,
         )
 
-    def load_model(self, model_filename):
+    def load_model(self, model_filename = None):
         """Load the target model, or the default model if none is specified. 
         If `self.model` is defined, it will be returned instead. """
         if not model_filename and not self.model:
