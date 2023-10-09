cc30eb2248c78c716744d56774d8f8bb5c383eaa
db0 <mail@dbzer0.com>
2023-10-09 18:12:38 +0200

    models_in_hash

--- horde/apis/v2/base.py
+++ horde/apis/v2/base.py
@@ -172,6 +172,7 @@ class GenerateTemplate(Resource):
         This is needed because some parameters are injected into the dict for the model, during runtime.
         So we need the logic of each GenerateTemplate class to be able to override this class to adjust the params dict accordingly.
         '''
+        logger.debug("wut")
         gen_payload = self.params.copy()
         gen_payload["models"] = self.models
         params_hash = hash_dictionary(gen_payload)
