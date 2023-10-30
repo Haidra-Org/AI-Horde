258580d7e36aafb70dbf09f9c7438c798856f404
db0 <mail@dbzer0.com>
2023-10-30 21:44:17 +0100

    draft

--- horde/apis/models/stable_v2.py
+++ horde/apis/models/stable_v2.py
@@ -30,9 +30,9 @@ class ImageModels(v2.Models):
 
         super().__init__(api)
         self.input_model_job_submit_metadata = api.model('SubmitInputMetaStable', {
-            'name': fields.String(description="The name of the metadata field"),
-            'type': fields.String(enum=["lora", "ti", "censorship", "img2img"], description="The relevance of the metadata field"),
+            'type': fields.String(enum=["lora", "ti", "censorship", "source_image", "source_mask"], description="The relevance of the metadata field"),
             'value': fields.String(["download_failed", "parse_failed", "baseline_mismatch"], description="The value of the metadata field"),
+            'ref': fields.String(required=False, description="Optioanlly a reference for the metadata (e.g. a lora ID)"),
         })
         self.response_model_generation_result = api.inherit('GenerationStable', self.response_model_generation_result, {
             'img': fields.String(title="Generated Image", description="The generated image as a Base64-encoded .webp file."),
