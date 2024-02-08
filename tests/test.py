4da549067b2e2c907c50c5cdf9a0b68fd487bb31
db0 <mail@dbzer0.com>
2024-02-08 01:33:43 +0100

    first working test

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,9 @@
 import requests
+import json
 
 CIVERSION = "0.1.1"
 HORDE_URL = "dev.stablehorde.net"
+TEST_MODELS = ["Fustercluck", "AlbedoBase XL (SDXL)"]
 
 def test_simple_image_gen() -> None:
     headers = {
@@ -20,17 +22,18 @@ def test_simple_image_gen() -> None:
         "steps": 8,
         "cfg_scale": 1.5,
         "sampler_name": "k_euler_a",
-        "models": ["Fustercluck", "AlbedoBase XL (SDXL)"],
+        "models": TEST_MODELS,
         "loras": [{"name": "247778", "is_version": True}],
     }    
     async_req = requests.post(f'https://{HORDE_URL}/api/v2/generate/async', json = async_dict, headers = headers)
     assert async_req.ok
-    submit_results = async_req.json()
-    req_id = submit_results['id']
+    async_results = async_req.json()
+    req_id = async_results['id']
+    # print(async_results)
     pop_dict = {
         "name": "CI/CD Fake Worker",
         "models": ["Fustercluck", "AlbedoBase XL (SDXL)"],
-        "bridge_agent": f"aihorde_ci_worker:{CIVERSION}:(discord)db0#1625",
+        "bridge_agent": f"AI Horde Worker reGen:4.1.0-citests:https://github.com/Haidra-Org/horde-worker-reGen",
         "amount": 10,
         "max_pixels": 4194304,
         "allow_img2img": True,
@@ -41,9 +44,10 @@ def test_simple_image_gen() -> None:
         "allow_lora": True        
     }
     pop_req = requests.post(f'https://{HORDE_URL}/api/v2/generate/pop', json = pop_dict, headers = headers)
-    print(pop_req)
     assert pop_req.ok
     pop_results = pop_req.json()
+    # print(json.dumps(pop_results, indent=4))
+
     job_id = pop_results['id']
     assert job_id is not None
     submit_dict = {
@@ -52,14 +56,25 @@ def test_simple_image_gen() -> None:
         "state": "ok",
         "seed": 0,
     }
-    submit_req = requests.post(f'https://{HORDE_URL}/api/v2/generate/pop', json = submit_dict, headers = headers)
+    submit_req = requests.post(f'https://{HORDE_URL}/api/v2/generate/submit', json = submit_dict, headers = headers)
     assert submit_req.ok
-    submit_results = pop_req.json()
-    assert submit_results["kudos"] > 0
+    submit_results = submit_req.json()
+    assert submit_results["reward"] > 0
     retrieve_req = requests.get(f'https://{HORDE_URL}/api/v2/generate/status/{req_id}', headers = headers)
     assert retrieve_req.ok
-    retrieve_req = async_req.json()
-    print(retrieve_req)
+    retrieve_results = retrieve_req.json()
+    # print(json.dumps(retrieve_results,indent=4))
+    assert len(retrieve_results['generations']) == 1
+    gen = retrieve_results['generations'][0]
+    assert len(gen['gen_metadata']) == 0
+    assert gen['seed'] == "0"
+    assert gen['worker_name'] == "CI/CD Fake Worker"
+    assert gen['model'] in TEST_MODELS
+    assert gen['state'] == 'ok'
+    assert retrieve_results['kudos'] > 1
+    assert retrieve_results['done'] is True
 
 if __name__ == "__main__":
+    # retrieve_req = requests.get(f'https://{HORDE_URL}/api/v2/generate/status/6b2e9602-472f-41cd-9cd7-bb2bb5062919')
+    # print(retrieve_req.text)
     test_simple_image_gen()
