80b1f1b3ed09b08b22b2c3e0e02de2cb1befc1ff
tazlin <tazlin.on.github@gmail.com>
2024-06-10 08:46:17 -0400

    fix: db `schedule_cron_job` now updates too
    If the cron string (`p_schedule`) is different than the currently scheduled cron job, `schedule_cron_job` now makes sure that the schedule is updated.

--- sql_statements/cron/schedule_cron_job.sql
+++ sql_statements/cron/schedule_cron_job.sql
@@ -7,20 +7,27 @@ CREATE OR REPLACE PROCEDURE schedule_cron_job(
 LANGUAGE plpgsql
 AS $$
 DECLARE
-  job_exists boolean;
+  existing_schedule TEXT;
+  existing_jobid INT;
 BEGIN
   SET search_path TO cron, public;
-  -- Check if the job already exists
-  SELECT EXISTS (
-    SELECT 1 FROM cron.job 
-    WHERE command = format($CRON$ CALL %s(); $CRON$, p_stored_procedure)
-  ) INTO job_exists;
-  
+
+  -- Get the existing schedule and jobid for the stored procedure
+  SELECT schedule, jobid
+  INTO existing_schedule, existing_jobid
+  FROM cron.job
+  WHERE command = format($CRON$ CALL %s(); $CRON$, p_stored_procedure);
+
+  -- If the job exists and the schedules don't match, update it
+  IF FOUND AND existing_schedule <> p_schedule THEN
+    PERFORM cron.unschedule(existing_jobid);
+    PERFORM cron.schedule(p_schedule, format($CRON$ CALL %s(); $CRON$, p_stored_procedure));
+    RAISE NOTICE 'Cron job schedule updated successfully for stored procedure: %', p_stored_procedure;
   -- If the job doesn't exist, schedule it
-  IF NOT job_exists THEN
+  ELSIF NOT FOUND THEN
     PERFORM cron.schedule(p_schedule, format($CRON$ CALL %s(); $CRON$, p_stored_procedure));
     RAISE NOTICE 'Cron job scheduled successfully for stored procedure: %', p_stored_procedure;
   ELSE
-    RAISE NOTICE 'Cron job already exists for stored procedure: %. Skipping scheduling.', p_stored_procedure;
+    RAISE NOTICE 'Cron job already exists with the same schedule for stored procedure: %. Skipping scheduling.', p_stored_procedure;
   END IF;
 END $$;
