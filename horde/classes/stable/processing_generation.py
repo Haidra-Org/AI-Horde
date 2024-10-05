f6952ed9e3d48c84c3f22d33546aade7716a0b86
db0 <mail@dbzer0.com>
2024-10-05 21:46:56 +0200

    fix: ensure ttl is always at least 150s

--- horde/classes/stable/processing_generation.py
+++ horde/classes/stable/processing_generation.py
@@ -168,6 +168,8 @@ class ImageProcessingGeneration(ProcessingGeneration):
         # Flux is way slower than Stable Diffusion
         if any(model_reference.get_model_baseline(mn) in ["flux_1"] for mn in self.wp.get_model_names()):
             self.job_ttl = self.job_ttl * 3
+        if self.job_ttl < 150:
+            self.job_ttl = 150
         if self.worker.extra_slow_worker is True:
             self.job_ttl = self.job_ttl * 3
         db.session.commit()
