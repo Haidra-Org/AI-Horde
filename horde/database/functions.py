aeb702490510533f5d4176b58fe1327353126056
db0 <mail@dbzer0.com>
2025-06-23 22:59:01 +0200

    get owner from passkey

--- horde/database/functions.py
+++ horde/database/functions.py
@@ -1678,11 +1678,9 @@ def get_worker_messages(user_id=None, worker_id=None, validity="all", page=0):
 
 def get_all_users_passkeys():
     """Retrieves all users passkeys."""
-    q = (
-        db.session.query(User.proxy_passkey, User.id, User.flagged).filter(
-            User.proxy_passkey.is_not(None),
-            User.flagged.is_(False),
-        ),
+    q = db.session.query(User.proxy_passkey, User.id, User.flagged).filter(
+        User.proxy_passkey.is_not(None),
+        User.flagged.is_(False),
     )
     logger.debug(q.all())
     return {
